a1357839edea4b60103d3f4149c98477f9db83f4
cxm6467@gmail.com <cxm6467@gmail.com>
2017-08-21 02:46:05 +0000

    Yay, db reset worked

--- db/migrate/20170820165158_create_sections.rb
+++ db/migrate/20170820165158_create_sections.rb
@@ -2,9 +2,9 @@ class CreateSections < ActiveRecord::Migration[5.0]
   def change
     create_table :sections do |t|
       t.string :title
-      t.integer :courses_id
+      t.integer :course_id
       t.timestamps
     end
-    add_index :sections, :courses_id
+    add_index :sections, :course_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -44,10 +44,10 @@ ActiveRecord::Schema.define(version: 20170820165158) do
 
   create_table "sections", force: :cascade do |t|
     t.string   "title"
-    t.integer  "courses_id"
+    t.integer  "course_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["courses_id"], name: "index_sections_on_courses_id", using: :btree
+    t.index ["course_id"], name: "index_sections_on_course_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
