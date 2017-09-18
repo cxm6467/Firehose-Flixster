f53e1a6363ac055768e272f6f5bbce1cabcf051f
cxm6467@gmail.com <cxm6467@gmail.com>
2017-09-18 03:44:57 +0000

    Better

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,43 +1,58 @@
 class Instructor::SectionsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+    before_action :authenticate_user!
+    before_action :require_authorize_for_current_course, only: [ :create]
+    before_action :require_authorize_for_current_section, only: [:update]
+    
+    def new
+        @course = Course.find(params[:course_id])
+        @section = Section.new
 
-  def new
-    @section = Section.new
-  end
-
-  def create
-    @section = current_course.sections.create(section_params)
-    redirect_to instructor_course_path(current_course)
-  end
-
-  def update
-    current_section.update_attributes(section_params)
-  end
-
-  private
-
-  def require_authorized_for_current_course
-    if current_course.user != current_user
-      render plain: "Unauthorized", status: :unauthorized
     end
-  end
 
-  helper_method :current_course
+    
+    def create
+        @section = current_course.sections.create(section_params)
+        redirect_to instructor_course_path(current_course)
+    end
+    
+    def update
+        current_section.update_attributes(section_params)
+        render text: "updated!"
+    end
+    
+    
+private
 
-  def current_section
-    @section ||= Section.find(params[:id])
-  end
-  
-  def current_course
+helper_method :current_course
+helper_method :current_section
+
+def current_course
     if params[:course_id]
-      @current_course ||= Course.find(params[:course_id])
+        @current_course ||= Course.find(params[:course_id])
     else
-      current_section.course
+        current_section.course
     end
-  end
+end
 
-  def section_params
+def require_authorize_for_current_section
+    if current_section.course.user != current_user
+        render text: 'Unauthorized', status: :unauthorized
+    end
+end
+
+def require_authorize_for_current_course
+    if current_course.user != current_user
+        render text: 'Unauthorized!', status: :unauthorized
+    end
+end
+
+
+def section_params
     params.require(:section).permit(:title, :row_order_position)
-  end
+end
+
+def current_section
+    @current_section ||= Section.find(params[:id])
+end
+
 end
